6ddb5aa4b83077c748b45081bf5eae1f0f090739
MikleMikle <mmchigorin@gmail.com>
2018-09-24 20:39:28 +0300

    Refractioned the code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+//Variable
 const cards = document.querySelectorAll('.memory-card');
 
 //To check what the card was clicked on
@@ -7,6 +8,12 @@ let firstCard, secondCard;
 
 
 
+// EventListeners 
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+//Functions
 
 function flipCard() {
    //instead of event.target I can use this key word
@@ -16,29 +23,33 @@ function flipCard() {
         //first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+
+        return; //it will stop it
+    } 
         //second click
         hasFlippedCard = false;
         secondCard = this;
 
-        console.log(firstCard.dataset.framework); //displays the value of an attribute
-        console.log(secondCard.dataset.framework);
-
-        //Do the cards march ?
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            //It's a match!
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            //if it's not a match
-            setTimeout(() => {  // thi gives me a time to flip the card
-                firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            }, 1500);
-        }
-    }
+        checkForMatch()
 }
 
+function checkForMatch() {
+    //Do the cards march ?
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+    //Using of ternary operator instead of if loop
+    isMatch ? disableCards() : unflipCards();
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {  // thi gives me a time to flip the card
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
